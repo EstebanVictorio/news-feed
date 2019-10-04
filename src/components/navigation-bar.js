9e868b17af8b85ee38e36a3cec1170e0a3581d0b
Esteban Victorio <estebanvictorio92@gmail.com>
2019-10-04 17:14:26 -0500

    [update] update nav bar styles

--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -4,10 +4,24 @@ const StyledNavigationBar = styled.nav`
   .navigation-bar__list {
     display: flex;
     list-style-type: none;
+    flex-wrap: wrap;
   }
 
   .navigation-bar__list-item {
-    margin: 0 12px;
+    margin: 12px;
+  }
+
+  .navigation-bar__link {
+    padding: 5px;
+    font-size: 22px;
+    font-weight: bold;
+    box-sizing: border-box;
+    border: 1px solid whitesmoke;
+    border-top-right-radius: 15px;
+  }
+
+  .navigation-bar__link:visited {
+    color: whitesmoke;
   }
 `
 
@@ -15,10 +29,14 @@ const NavigationBar = () => (
   <StyledNavigationBar>
     <ul className="navigation-bar__list">
       <li className="navigation-bar__list-item">
-        <Link to="/">Your location country</Link>
+        <Link className="navigation-bar__link" to="/">
+          Your location country
+        </Link>
       </li>
       <li className="navigation-bar__list-item">
-        <Link to="/international">International</Link>
+        <Link className="navigation-bar__link" to="/international">
+          International
+        </Link>
       </li>
     </ul>
   </StyledNavigationBar>
